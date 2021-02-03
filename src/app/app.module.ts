2a56aca62e2aa4a927a13af07eff87372ecf9ded
jaimesvlucas <jaimesanchez456@gmail.com>
2021-02-03 20:49:11 +0100

    Version 1.0 primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
